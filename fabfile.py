f05c8e8e3118ce10421668ee3311a33ed361e378
Jonathan Laperle <jonathanlaperle@icloud.com>
2015-10-27 11:13:52 -0400

    added deploy function to fabric

--- fabfile.py
+++ fabfile.py
@@ -8,3 +8,8 @@ def restart():
     with cd('/home/galaxy/galaxy-dist'):
         run('GALAXY_RUN_ALL=1 sh run.sh --stop-daemon')
         run('GALAXY_RUN_ALL=1 sh run.sh --daemon')
+
+@hosts('galaxy')
+def deploy_vm():
+    with cd('/home/galaxy/geec_galaxy'):
+        run('git pull')
