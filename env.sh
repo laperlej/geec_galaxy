ef9a53b8f93f6d5e5da614afd4480c42a05ad629
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2016-10-18 17:21:22 -0400

    new website

---
+++ env.sh
@@ -0,0 +1,5 @@
+module use /cvmfs/soft.mugqic/CentOS6/modulefiles/
+module use /cvmfs/opt.usherbrooke.ca/CentOS6/Modules/modulefiles.x86_64/
+module use /cvmfs/opt.usherbrooke.ca/modulesfiles
+module use /opt/Modules/modulefiles
+module load python64/2.7.5 gcc/5.2.0 intel64/12.0.5.220 openmpi_intel64/1.4.3_ofed hdf5/1.8.16 boost64/1.61.0 szip/2.1 cmake/2.8.8 R/3.0.1
