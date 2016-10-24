146703f47ab06f8bff5900f0ff23cc549a9f2206
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2016-10-24 12:50:42 -0400

    new interface

--- env.sh
+++ env.sh
@@ -1,6 +1,7 @@
-source /cvmfs/bioinformatics.usherbrooke.ca/galaxy/prog/lmod/lmod/init/profile
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cvmfs/opt.usherbrooke.ca/CentOS6/szip/2.1/lib/
+source /cvmfs/opt.usherbrooke.ca/lmod/lmod/init/profile
 module use /cvmfs/soft.mugqic/CentOS6/modulefiles/
 module use /cvmfs/opt.usherbrooke.ca/CentOS6/Modules/modulefiles.x86_64/
 module use /cvmfs/opt.usherbrooke.ca/modulesfiles
 module use /opt/Modules/modulefiles
-module load python64/2.7.5 gcc/5.2.0 intel64/12.0.5.220 openmpi_intel64/1.4.3_ofed hdf5/1.8.16 boost64/1.61.0 szip/2.1 cmake/2.8.8 R/3.0.1
+module load python64/2.7.5 gcc/5.2.0 intel64/12.0.5.220 openmpi_intel64/1.4.3_ofed hdf5/1.8.16 boost64/1.61.0 cmake/2.8.8 R/3.0.1
