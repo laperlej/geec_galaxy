a4b9cad6e79ea536aa2eeb87560ff055d95436c1
Simon Hébert-Deschamps <hebs2011@dbio-d83015-01b.DInf.fsci.usherbrooke.ca>
2017-07-10 14:20:05 -0400

    add geec_analysis wrapper

---
+++ geec_ari.py
@@ -0,0 +1,18 @@
+import sys
+import subprocess
+import os.path
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'geec_tools/python/utils'))
+import config
+
+def geec_annotate(args):
+    arguments = [config.GEEC_ARI] + args
+
+    subprocess.call(arguments)
+
+def main():
+    args = sys.argv[1:]
+    geec_annotate(args)
+
+if __name__ == '__main__':
+    main()
