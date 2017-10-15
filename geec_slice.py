bce28023f52197049335c7784c7fb64ad938c2f9
simonisateur <simonisateur@gmail.com>
2017-10-15 15:46:35 -0400

    fix imports

--- geec_slice.py
+++ geec_slice.py
@@ -3,7 +3,7 @@ import subprocess
 import os.path
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'geec_tools/python/utils'))
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'geec_tools/geec_slice'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'geec_tools/geec_analysis'))
 import config
 import geec_slice
 
