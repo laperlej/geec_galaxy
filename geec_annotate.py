0007d924383ff992f0916d59fbe57ba1bbdacc47
simonisateur <simonisateur@gmail.com>
2017-10-15 14:51:30 -0400

    fix Galaxy permisssion to subprocess call

--- geec_annotate.py
+++ geec_annotate.py
@@ -3,16 +3,18 @@ import subprocess
 import os.path
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'geec_tools/python/utils'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'geec_tools/geec_analysis'))
 import config
+import geec_annotate
 
-def geec_annotate(args):
+def run_geec_annotate(args):
     arguments = [config.GEEC_ANNOTATE] + args
 
-    subprocess.call(arguments)
+    geec_annotate.main(arguments)
 
 def main():
     args = sys.argv[1:]
-    geec_annotate(args)
+    run_geec_annotate(args)
 
 if __name__ == '__main__':
     main()
--- geec_slice.py
+++ geec_slice.py
@@ -3,16 +3,18 @@ import subprocess
 import os.path
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'geec_tools/python/utils'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'geec_tools/geec_slice'))
 import config
+import geec_slice
 
-def geec_annotate(args):
+def run_geec_slice(args):
     arguments = [config.GEEC_SLICE] + args
 
-    subprocess.call(arguments)
+    geec_slice.main(arguments)
 
 def main():
     args = sys.argv[1:]
-    geec_annotate(args)
+    run_geec_slice(args)
 
 if __name__ == '__main__':
     main()
