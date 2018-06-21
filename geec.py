2809117ce531d1f4e8376658b9bb0c445ff7b03c
laperlej <jonathan.laperle@usherbrooke.ca>
2018-06-21 14:13:59 -0400

    debug

--- geec.py
+++ geec.py
@@ -315,7 +315,7 @@ def main():
     if args.labels == ['None']:
         args.labels = []
 
-    desc = "{0}_{1}_{2}_{3}_{4}_{5}datasets".format(args.bin, args.assembly, args.include, args.exclude, args.metric, len(args.files))
+    desc = "assembly={1}, bin={0}, nbUserDatasets={5}, include={2}, exclude={3}, metric={4}”".format(args.bin, args.assembly, args.include, args.exclude, args.metric, len(args.files))
     #desc = ""
 
     #read md5 list file for public data
