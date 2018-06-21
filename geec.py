f5a0487f201b237a7345377e6f3953d4a4ddc054
laperlej <jonathan.laperle@usherbrooke.ca>
2018-06-21 10:06:03 -0400

    debug

--- geec.py
+++ geec.py
@@ -367,7 +367,7 @@ def main():
         else:
             input_list_path2 = create_input_list(input_list2)
             precalc_matrix = get_matrix(args.assembly, args.bin, args.include, args.exclude)
-            slice_matrix([x[1] for x in input_list2], args.assembly, args.bin, args.include, args.exclude, precalc_matrix)
+            slice_matrix([x[1] for x in input_list2], args.assembly, args.bin, args.include, args.exclude, mat_file_nn)
             launch_make_matrix(mat_file_nn, args.output, args.md5s)
     else:
         input_list_path = create_input_list(input_list1 + input_list2)
