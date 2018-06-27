d54aa7d1986ea9f34b430974144bd52341d0a4c9
laperlej <jonathan.laperle@usherbrooke.ca>
2018-06-27 10:45:06 -0400

    added nb public files in desc

--- geec.py
+++ geec.py
@@ -323,9 +323,6 @@ def main():
     if args.labels == ['None']:
         args.labels = []
 
-    desc = "assembly={1}, bin={0}, nbUserDatasets={5}, include={2}, exclude={3}, metric={4}".format(args.bin, args.assembly, args.include, args.exclude, args.metric, len(args.files))
-    #desc = ""
-
     #read md5 list file for public data
     if args.md5s:
       md5_json = json.load(open(args.md5s))
@@ -333,6 +330,8 @@ def main():
     else:
       md5_json = {}
       md5s = []
+
+    desc = "assembly={1}, bin={0}, nbUserDatasets={5}, nbPublicDatasets={6}, include={2}, exclude={3}, metric={4}".format(args.bin, args.assembly, args.include, args.exclude, args.metric, len(args.files), md5_json["count"])
 
     md5_json = listjson2dictjson(md5_json)
 
